d7220b5b9ba4a36016178645ba343814a8d831fa
Anuj Soni <anujsoni08@gmail.com>
2020-06-20 20:11:39 +0530

    fixed swipe

--- src/component/Slider/Slider.js
+++ src/component/Slider/Slider.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import { useSwipeable } from "react-swipeable";
+import { Swipeable } from "react-swipeable";
 
 import {
   SliderItem,
@@ -19,13 +19,6 @@ const Slider = (props) => {
   const [currentIndex, setCurrentIndex] = useState(0);
 
   const width = useWindowWidth();
-
-  const swipeHandlers = useSwipeable({
-    onSwipedLeft: () => prevSlide(),
-    onSwipedRight: () => nextSlide(),
-    preventDefaultTouchmoveEvent: true,
-    trackMouse: true,
-  });
 
   function useWindowWidth() {
     const [width, setWidth] = useState(window.innerWidth);
@@ -68,60 +61,61 @@ const Slider = (props) => {
 
   return (
     <div>
-      <SliderContainer
-        {...swipeHandlers}
-        className={"slider-instance"}
-        height={"500px"}
-      >
-        <SliderWrapper
-          {...swipeHandlers}
-          width={width * images.length}
-          style={{
-            transform: `translateX(${-(currentIndex * width)}px)`,
-            transition: "transform ease-out 0.30s",
-            width: width * images.length + "px",
-          }}
+      <SliderContainer className={"slider-instance"} height={"500px"}>
+        <Swipeable
+          onSwipedLeft={prevSlide}
+          onSwipedRight={nextSlide}
+          preventDefaultTouchmoveEvent={true}
+          trackMouse={true}
         >
-          {images.map((i, index) => {
-            return (
-              <Slide
-                key={index}
-                last={index === images.length - 1}
-                index={index}
-                currentIndex={currentIndex}
-                item={i}
-                width={width}
-              />
-            );
-          })}
-        </SliderWrapper>
+          <SliderWrapper
+            width={width * images.length}
+            style={{
+              transform: `translateX(${-(currentIndex * width)}px)`,
+              transition: "transform ease-out 0.30s",
+              width: width * images.length + "px",
+            }}
+          >
+            {images.map((i, index) => {
+              return (
+                <Slide
+                  key={index}
+                  last={index === images.length - 1}
+                  index={index}
+                  currentIndex={currentIndex}
+                  item={i}
+                  width={width}
+                />
+              );
+            })}
+          </SliderWrapper>
+          <BulletList style={{ display: bulletStatus ? "flex" : "none" }}>
+            {images.map((i, index) => {
+              return (
+                <BulletListItem
+                  active={index === currentIndex}
+                  onClick={() => onBulletClick(index)}
+                  key={index}
+                >
+                  &nbsp;
+                </BulletListItem>
+              );
+            })}
+          </BulletList>
+          <div style={{ display: arrowStatus ? "block" : "none" }}>
+            {currentIndex > 0 ? (
+              <ControlLeft onClick={prevSlide}>prev</ControlLeft>
+            ) : (
+              ""
+            )}
 
-        <BulletList style={{ display: bulletStatus ? "flex" : "none" }}>
-          {images.map((i, index) => {
-            return (
-              <BulletListItem
-                active={index === currentIndex}
-                onClick={() => onBulletClick(index)}
-                key={index}
-              >
-                &nbsp;
-              </BulletListItem>
-            );
-          })}
-        </BulletList>
-        <div style={{ display: arrowStatus ? "block" : "none" }}>
-          {currentIndex > 0 ? (
-            <ControlLeft onClick={prevSlide}>prev</ControlLeft>
-          ) : (
-            ""
-          )}
-
-          {currentIndex < images.length - 1 ? (
-            <ControlRight onClick={nextSlide}>next</ControlRight>
-          ) : (
-            ""
-          )}
-        </div>
+            {currentIndex < images.length - 1 ? (
+              <ControlRight onClick={nextSlide}>next</ControlRight>
+            ) : (
+              ""
+            )}
+          </div>
+        </Swipeable>
       </SliderContainer>
       <button onClick={() => setArrowStatus(!arrowStatus)}>
         {" "}
